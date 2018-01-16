a7c3b3a77f463c21a927b6df6f5b1a92757f933d
pepa65 <solusos@passchier.net>
2018-01-16 18:09:33 +0700

    Prepare difth for deployment as webpage with difth.php and index.html

--- difth.php
+++ difth.php
@@ -1,5 +1,7 @@
 <?php
-echo(shell_exec('./difth' . ($_POST['send'] == '⇶' ? ' -n' : '')
+// For the user that is used by the webserver: the directory $files (in difth)
+//  must be writable, and 'difth' must be in path.
+echo(shell_exec('difth' . ($_POST['send'] == '⇶' ? ' -n' : '')
 	. ' -s "' . str_replace("\"", "\\\"", $_POST['old']) . '" "'
 	. str_replace("\"", "\\\"", $_POST['new']) . '" /'));
 ?>
