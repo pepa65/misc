47ef4638925ec809faef45a145715f108d373d42
pepa65 <solusos@passchier.net>
2018-01-16 20:18:52 +0700

    Add favicon, add installation instructions to difth.php

--- difth.php
+++ difth.php
@@ -1,6 +1,7 @@
 <?php
-// For the user that is used by the webserver: the directory $files (in difth)
-//  must be writable, and 'difth' must be in path.
+// For the user that is used by the webserver: 'difth' must be in path,
+//  and the directory $files (variable in difth) must be writable.
+// Copy these files to the webserver root: index.html difth.php favicon.ico
 echo(shell_exec('difth' . ($_POST['send'] == '⇶' ? ' -n' : '')
 	. ' -s "' . str_replace("\"", "\\\"", $_POST['old']) . '" "'
 	. str_replace("\"", "\\\"", $_POST['new']) . '" /'));
