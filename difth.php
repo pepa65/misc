0d6adfd8a45cb31d5c2e077423c6df7ae594d392
pepa65 <solusos@passchier.net>
2018-03-02 00:52:33 +0700

    Corrections

--- difth.php
+++ difth.php
@@ -1,6 +1,7 @@
 <?php
 // For the user that is used by the webserver: 'difth' must be in path,
-//  and the directory $files (variable in difth) must be writable.
+//  and the directory $files (variable set in difth) must be writable,
+//  chown www-data $files; chmod 2770 $files
 // Copy these files to the webserver root: index.html difth.php favicon.ico
 echo(shell_exec('difth' . ($_POST['send'] == '⇶' ? ' -n' : '')
 	. ' -s "' . str_replace("\"", "\\\"", $_POST['old']) . '" "'
